a9abf0d42a07afee43002c2a539c694a1118bc34
mama <seadragonmillion@gmail.com>
2020-04-04 21:33:41 +0800

    commit

--- SeleniumLearning/src/test/java/mouseHover/MouseMovement2.java
+++ SeleniumLearning/src/test/java/mouseHover/MouseMovement2.java
@@ -28,12 +28,13 @@ public class MouseMovement2 extends MouseAction
 		driver.get("http://executeautomation.com/blog/mouse-hover-click-selenium/");
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-		Thread.sleep(3000);
+		Thread.sleep(4000);
 		WebElement videoSeries = driver.findElement(By.xpath("(//a[contains(.,'Video Series')])[2]"));
 		WebElement freeCourses = driver.findElement(By.xpath("(//a[@href='http://executeautomation.com/blog/free-courses/'])[2]"));
 		WebElement titBits = driver.findElement(By.xpath("(//a[@href='http://executeautomation.com/blog/selenium-titbits/'])[2]"));
+		Thread.sleep(4000);
 		mouse_hover(driver, videoSeries);
-		Thread.sleep(2000);
+		Thread.sleep(4000);
 
 		mouse_hover_click(driver, freeCourses,titBits);
 		
